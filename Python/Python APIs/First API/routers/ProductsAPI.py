f56a86db13703ced6e3aba0385135f12ccd32dbb
Yousef alhamad <yousefalhamad9@gmail.com>
2026-07-27 00:38:45 +0300

    Added new endpoints for productsAPI

--- Python/Python APIs/First API/routers/ProductsAPI.py
+++ Python/Python APIs/First API/routers/ProductsAPI.py
@@ -13,11 +13,22 @@ class ProductCreateDTO(BaseModel):
       stock:int
       attributes:Dict[str,Any]
 
+class ProductUpdateDTO(BaseModel):
+      id:str
+      name:str
+      category:str
+      price:float
+      stock:int
+      attributes:Dict[str,Any]      
+
 
 @router.post("/Create-Product",status_code=status.HTTP_201_CREATED,response_model=Product)
-async def createProduct(payload: ProductCreateDTO):
+async def createProduct(payload: ProductCreateDTO): 
 
-    lastProduct=await Product.find_all().sort("-id").first_or_none()
+
+
+
+    lastProduct=await Product.find_all().sort("-_id").first_or_none()
     if lastProduct:
 
         lastNumber=int(lastProduct.id.split("-")[1])
@@ -32,13 +43,82 @@ async def createProduct(payload: ProductCreateDTO):
     
     try:
         newProduct=Product(
-                id=newProductID,name=payload.name,category=payload.category,price=payload.price
+                 id=newProductID,name=payload.name,category=payload.category,price=payload.price
                 ,stock=payload.stock,attributes=payload.attributes
                 )
+         
         await newProduct.insert()
 
-    except Exception:
-        raise HTTPException(status_code=500,detail="Server Issue")
+    except Exception as exception:
+        raise HTTPException(status_code=500,detail=f"Server Issue: {str(exception)}")
 
     
     return newProduct
+
+@router.delete("/Delete-Product",status_code=status.HTTP_200_OK)
+async def deleteProduct(productID:str):
+      
+     
+  
+     try:
+        product = await Product.get(productID)
+        if not product :
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Product Not Found")
+           
+        await product.delete()
+        return {"message":f"Product {productID} delete successfully!"}
+     except Exception as exception:
+        raise HTTPException(status_code=500,detail=f"Server Issue: {str(exception)}")
+
+
+@router.get("/ProductsList",response_model=List[Product])
+async def GetCustomers():
+    products=await Product.find_all().to_list()
+    
+    if products ==None :
+        raise HTTPException(status_code=500,detail="Server Problem")
+
+    return products
+@router.put("/UpdateProduct",status_code=status.HTTP_200_OK)
+
+async def updateProduct(payload:ProductUpdateDTO):
+
+    try:
+        product=await Product.get(payload.id)
+
+        if product ==None:
+             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Product does not exist")
+        
+        product.name=payload.name
+        product.attributes=payload.attributes
+        product.stock=payload.stock
+        product.price=payload.price
+        product.category=payload.category
+
+        #await product.update()
+        await product.save()
+
+        return {"message":"Product Updated successfully!"}
+    except Exception as exception:
+        raise HTTPException(status_code=500,detail=f"Server Issue: {str(exception)}")
+
+
+class UpdateCategoryDTO(BaseModel):
+    id:str
+    category:str
+
+@router.patch("/UpdateCategory",status_code=status.HTTP_200_OK)    
+async def UpdateCategory(payload:UpdateCategoryDTO):
+
+    try:
+        product =await Product.get(payload.id)
+
+        if product==None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Product does not exist")
+
+        product.category=payload.category
+
+        await product.save()
+        return {"message":"Category Updated successfully!"}
+    except Exception as exception:
+        raise HTTPException(status_code=500,detail=f"Server Issue: {str(exception)}")        
