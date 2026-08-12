882d9481bcd0c4c07043f4b91cc0439aa79493bc
YousefAlhamad27 <yousefalhamad9@gmail.com>
2026-08-13 01:04:37 +0300

    Update auth implementation

--- src/Auth/security.py
+++ src/Auth/security.py
@@ -1,5 +1,16 @@
 
+from fastapi import HTTPException
+from fastapi.params import Depends
 from fastapi.security import OAuth2PasswordBearer
+from src import  Util
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="UserAPI/login")
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+   
+    user =  Util.verify_access_token(token)
+    if user is None:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    return user
